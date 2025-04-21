23aa6b74e7aba4e398c433491b12bab7099349a7
Ricardo Cremonese <ricardocremonese@Don.local>
2025-04-21 15:14:38 -0300

    ultimaversao

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,1 +1,7 @@
-// Cliente Supabase configurado com chave e URL do Don
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://mlmqhtvjhqkkaehgeomc.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sbXFodHZqaHFra2FlaGdlb21jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwODQwNDgsImV4cCI6MjA2MDY2MDA0OH0._HIuEJKtDvbzNVyLd2eGrCbrs6uIidS9-JlN-ZFXNNs'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
